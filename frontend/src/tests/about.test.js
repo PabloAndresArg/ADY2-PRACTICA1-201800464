1c6a91654f46f0bb72e23167e2187a96702e711c
pabloandresarg <3004456600101@ingenieria.usac.edu.gt>
2022-03-02 17:09:18 -0600

    fix error 1

--- frontend/src/tests/about.test.js
+++ frontend/src/tests/about.test.js
@@ -7,10 +7,9 @@ import About from "../componentes/Paginas/About";
 describe("Tests in <About />", () => {
 
     test("should message 201800464 - Pablo Andres Argueta Hernandez  appear on the screen", () => {
-        const textoEsperado = "201800464 - Pablo Andres Argueta HernandezR";
+        const textoEsperado = "201800464 - Pablo Andres Argueta Hernandez";
         const wrapper = shallow(<About />);
         const textoInput = wrapper.find("h2").at(0).text();
-        console.log(textoInput)
         expect(textoInput).toBe(textoEsperado);
     });
 });
