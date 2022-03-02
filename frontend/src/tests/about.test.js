163788af8b04a93a4a41ac66c8c9210703c2381b
pabloandresarg <3004456600101@ingenieria.usac.edu.gt>
2022-03-02 17:05:55 -0600

    tests fallidos

--- frontend/src/tests/about.test.js
+++ frontend/src/tests/about.test.js
@@ -7,7 +7,7 @@ import About from "../componentes/Paginas/About";
 describe("Tests in <About />", () => {
 
     test("should message 201800464 - Pablo Andres Argueta Hernandez  appear on the screen", () => {
-        const textoEsperado = "201800464 - Pablo Andres Argueta Hernandez";
+        const textoEsperado = "201800464 - Pablo Andres Argueta HernandezR";
         const wrapper = shallow(<About />);
         const textoInput = wrapper.find("h2").at(0).text();
         console.log(textoInput)
