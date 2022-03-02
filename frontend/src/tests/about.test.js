8fc1eae4ec290cec7f2de2ae2318d4eacb948958
pabloandresarg <3004456600101@ingenieria.usac.edu.gt>
2022-03-02 15:34:57 -0600

    pruebas

---
+++ frontend/src/tests/about.test.js
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { shallow } from "enzyme";
+import React from "react";
+import About from "../componentes/Paginas/About";
+
+
+describe("Tests in <About />", () => {
+
+    test("should message 201800464 - Pablo Andres Argueta Hernandez  appear on the screen", () => {
+        const textoEsperado = "201800464 - Pablo Andres Argueta Hernandez";
+        const wrapper = shallow(<About />);
+        const textoInput = wrapper.find("h2").at(0).text();
+        console.log(textoInput)
+        expect(textoInput).toBe(textoEsperado);
+    });
+});
