817c748d1fd5f906d5eef3ec51949a2b372de363
pabloandresarg <3004456600101@ingenieria.usac.edu.gt>
2022-03-02 19:45:16 -0600

    FLUJO

--- frontend/src/componentes/Paginas/About.js
+++ frontend/src/componentes/Paginas/About.js
@@ -18,7 +18,7 @@ export default function AboutFull({ props }) {
       <div style={{ textAlign: 'center' }}>
         <h1>ANALISIS Y DISEÑO 2</h1>
         <h2>201800464 - Pablo Andres Argueta Hernandez</h2>
-        <h1>HIZO BIEN EL MOVE </h1>
+        <h1>PROBANDO TODO EL FLUJO DEVOPS </h1>
       </div>
     </div>
   );
